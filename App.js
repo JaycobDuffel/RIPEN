183b572a7de231315b8f1faf5a6ab55f0b1e057a
Jaycob Duffel <jaycobduffel@gmail.com>
2021-08-06 20:09:34 -0700

    Finish AuthContext

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import AuthContext from './src/Context/AuthContext'
 import HomeScreen from './src/Screens/HomeScreen'
 import AccountScreen from './src/Screens/AccountScreen'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState({firstName: 'TayJay'});
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
-          <Stack.Screen name="AccountScreen" component={AccountScreen} options={{headerShown: false}}  />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="AccountScreen" component={AccountScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
