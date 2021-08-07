8d7e56f6c3067899fc2e6112fb74a0bb5bbd0f33
Jaycob Duffel <jaycobduffel@gmail.com>
2021-08-06 17:29:26 -0700

    Implement basic react navigation

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/Screens/HomeScreen'
+import AccountScreen from './src/Screens/AccountScreen'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>RIPEN</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
+          <Stack.Screen name="AccountScreen" component={AccountScreen} options={{headerShown: false}}  />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
