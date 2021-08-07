27579940255e251f2600bd2c30fdda46b234724d
Jaycob Duffel <jaycobduffel@gmail.com>
2021-08-06 17:35:39 -0700

    Modify homescreen height

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import Constants from '../Constants/Constants'
 
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
         backgroundColor: Constants.styles.primaryColor,
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: '100%'
     },
 });
