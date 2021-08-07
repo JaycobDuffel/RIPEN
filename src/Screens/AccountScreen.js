224707929fa5ccfb33d371739b4f104381006301
Jaycob Duffel <jaycobduffel@gmail.com>
2021-08-06 17:34:43 -0700

    Create basic account screen

---
+++ src/Screens/AccountScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from '../Constants/Constants';
+
+export default function AccountScreen(props) {
+    return (
+        <View style={styles.container}>
+          <Text>Hello AccountScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        backgroundColor: Constants.styles.primaryColor,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent:'center',
+},
+});
