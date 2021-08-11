930e869125719d5b13a2ae156e9ede095de5eae6
Jaycob Duffel <jaycobduffel@gmail.com>
2021-08-10 19:38:20 -0700

    Create base login and register pages

---
+++ src/Screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function LoginScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>login screen</Text>
+        </View>
+    );
+}
