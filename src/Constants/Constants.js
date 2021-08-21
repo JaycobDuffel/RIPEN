ea36bece0b04a0aeb93338cb0a5bd37feba5e9c6
tivings <ivingstaylor@gmail.com>
2021-08-20 22:42:22 -0700

    Added additional base and accent fonts to give more options

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,10 +1,30 @@
 const Constants = {
     styles: {
-        brandYellow: '#e9c46a',
-        brandBlue: '#264653',
-        brandGreen: '#2a9d8f',
-        brandOrange: '#f4a261',
-        brandRed: '#e76f51'
+        brandYellow: {
+            300: '#f3deab',
+            500: '#e9c46a',
+            700: '#deab2d',
+        },
+        brandBlue: {
+            500: '#608492',
+            700: '#264653',
+            900: '040f13',
+        },
+        brandGreen: {
+            300: '#62beb3',
+            500: '#2a9d8f',
+            700: '027b6c',
+        },
+        brandOrange: {
+            300: 'f8c7a0',
+            500: '#f4a261',
+            700: 'ef7d23'
+        },
+        brandRed: {
+            300: 'f0a390',
+            500: '#e76f51',
+            700: '#dd3e16'
+        }
     }
 }
 
